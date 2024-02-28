1f57817d5459a93a3a01e099cc62c9f53a889f14
Kevin Wilson <104521683+KevinWilsonAT@users.noreply.github.com>
2024-02-28 17:22:03 -0300

    upload classes 28-02-24

---
+++ Module C - Understanding the DOM/aula008/ex007.js
@@ -0,0 +1,25 @@
+//   Relational Operators
+
+//   >     Greater
+//   <     Lesser
+//   >=    Greater or equal
+//   <=    Lesser or equal
+//   ==    Equal
+//   !=    Different
+
+//   5 >  2     true
+//   7 <  4     false
+//   8 >= 8     true
+//   9 <= 7     false
+//   5 == 5     true
+//   4 != 4     false
+
+// Identity
+
+//   5 == 5     true
+//   5 == '5'   true
+//   5 === '5'  false
+//   5 === 5    true
+
+//   5 != '5'   false
+//   5 !== '5'  true
