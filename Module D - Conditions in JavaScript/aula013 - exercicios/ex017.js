e808bdc537c20c9754ddd0a29c58c9f1a5e02fcf
Kevin Wilson <104521683+KevinWilsonAT@users.noreply.github.com>
2024-03-19 12:58:06 -0300

    update classes 19-03-24

---
+++ Module D - Conditions in JavaScript/aula013 - exercicios/ex017.js
@@ -0,0 +1,28 @@
+function load(){   
+    let msg = window.document.getElementById('msg')
+    let img = window.document.getElementById('img')
+    let date = new Date()
+    let hour = date.getHours()
+    let minute = date.getMinutes()
+
+    if (hour >= 0 && hour < 6){
+        img.src= './img/night.png'
+        document.body.style.background = '#141638'
+        msg.innerHTML = `Good Night. Now it's ${hour} hours and ${minute} minutes - (${hour}:${minute} AM)`
+    }
+    else if (hour >= 6 && hour < 12){
+        img.src= './img/morning.png'
+        document.body.style.background = '#F3F0E9'
+        msg.innerHTML = `Good Morning. Now it's ${hour} hours and ${minute} minutes - (${hour}:${minute} AM)`
+    }
+    else if (hour >= 12 && hour < 18){
+        img.src= './img/afternoon.png'
+        document.body.style.background = '#FDC389'
+        msg.innerHTML = `Good Afternoon. Now it's ${hour} hours and ${minute} minutes - (${hour}:${minute} PM)`
+    }
+    else {
+        img.src= './img/evening.png'
+        document.body.style.background = '#B886AB'
+        msg.innerHTML = `Good Evening. Now it's ${hour} hours and ${minute} minutes - (${hour}:${minute} PM)`
+    }
+}
