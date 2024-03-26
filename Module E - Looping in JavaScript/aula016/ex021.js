61fedfab53f91666f2c80796d3c55755f5a0d604
Kevin Wilson <104521683+KevinWilsonAT@users.noreply.github.com>
2024-03-26 11:34:45 -0300

    chore: Upload class 26-03-24

---
+++ Module E - Looping in JavaScript/aula016/ex021.js
@@ -0,0 +1,9 @@
+/*
+    LOOPING COMMAND WITH CONDITION CONTROL (FOR):
+
+        Syntax:
+        
+            for(<start>; <end>; <step>){
+                <commands_block1>
+            }
+*/
---
+++ Module E - Looping in JavaScript/aula016/ex022a.js
@@ -0,0 +1,3 @@
+for(var count = 0; count <= 6; count++){
+    window.alert("Using WHILE: Hello #" + count)
+}
