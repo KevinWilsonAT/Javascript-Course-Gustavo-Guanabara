b604906e19380290aacd4187b50d4077d9fc50af
Kevin Wilson <104521683+KevinWilsonAT@users.noreply.github.com>
2024-03-26 11:37:36 -0300

    chore: Update class 16-02-24 pt2
    removed semicolons to padronize all classes materials

--- Module A - Getting to know JavaScript/aula04/ex001.js
+++ Module A - Getting to know JavaScript/aula04/ex001.js
@@ -1,5 +1,5 @@
-window.alert("Hello World");
-window.alert("This is my First Message");
-window.confirm("Are you liking JavaScript?");
-window.prompt("What is your name:");
-window.prompt("How old are you?");
+window.alert("Hello World")
+window.alert("This is my First Message")
+window.confirm("Are you liking JavaScript?")
+window.prompt("What is your name:")
+window.prompt("How old are you?")
