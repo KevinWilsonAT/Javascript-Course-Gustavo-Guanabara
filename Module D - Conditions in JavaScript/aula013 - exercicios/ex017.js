2f0a04ecf925d70be8451cde3e8de5cc48c60702
Kevin Wilson <104521683+KevinWilsonAT@users.noreply.github.com>
2024-03-26 12:07:08 -0300

    chore: Update classes 19-03-24
    added blank lines to keep code clean

--- Module D - Conditions in JavaScript/aula013 - exercicios/ex017.js
+++ Module D - Conditions in JavaScript/aula013 - exercicios/ex017.js
@@ -8,21 +8,25 @@ function load(){
     if (hour >= 0 && hour < 6){
         img.src= './img/night.png'
         document.body.style.background = '#141638'
+
         msg.innerHTML = `Good Night. Now it's ${hour} hours and ${minute} minutes - (${hour}:${minute} AM)`
     }
     else if (hour >= 6 && hour < 12){
         img.src= './img/morning.png'
         document.body.style.background = '#F3F0E9'
+
         msg.innerHTML = `Good Morning. Now it's ${hour} hours and ${minute} minutes - (${hour}:${minute} AM)`
     }
     else if (hour >= 12 && hour < 18){
         img.src= './img/afternoon.png'
         document.body.style.background = '#FDC389'
+
         msg.innerHTML = `Good Afternoon. Now it's ${hour} hours and ${minute} minutes - (${hour}:${minute} PM)`
     }
     else {
         img.src= './img/evening.png'
         document.body.style.background = '#B886AB'
+
         msg.innerHTML = `Good Evening. Now it's ${hour} hours and ${minute} minutes - (${hour}:${minute} PM)`
     }
 }
