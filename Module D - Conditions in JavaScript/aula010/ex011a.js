7c9bf16737f69f5d0f969ab7889b94288d83ba08
Kevin Wilson <104521683+KevinWilsonAT@users.noreply.github.com>
2024-03-26 11:53:56 -0300

    chore: Update classes 02-03-24
    added blank lines to keep code clean

--- Module D - Conditions in JavaScript/aula010/ex011a.js
+++ Module D - Conditions in JavaScript/aula010/ex011a.js
@@ -5,4 +5,5 @@ window.alert(`The car speed is ${spd}Km/h`)
 if(spd > 60){
     window.alert("Your car exceeds the allowed speed. YOU WILL BE FINED")
 }
+
 window.alert("Always drive using safety belt")
--- Module D - Conditions in JavaScript/aula010/ex011b.js
+++ Module D - Conditions in JavaScript/aula010/ex011b.js
@@ -1,6 +1,7 @@
 var country = window.prompt("Your Country: ")
 
 window.alert(`Living in ${country}...`)
+
 if(country == "Brazil"){
     window.alert("... you are brazilian.")
 }
