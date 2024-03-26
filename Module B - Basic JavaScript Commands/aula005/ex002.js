33fa18b2ab38e1579f950bdeb4dd663ed323c862
Kevin Wilson <104521683+KevinWilsonAT@users.noreply.github.com>
2024-03-26 11:39:28 -0300

    chore: Update classes 16-02-24 pt2
    removed semicolons to padronize all classes materials

--- Module B - Basic JavaScript Commands/aula005/ex002.js
+++ Module B - Basic JavaScript Commands/aula005/ex002.js
@@ -1,9 +1,9 @@
-n = "google";
-window.alert(typeof(n));
-window.alert(typeof 6.5);
-window.alert(typeof "6.5");
-window.alert(typeof function(){});
-window.alert(typeof undefined);
-window.alert(typeof NaN);
-window.alert(typeof Infinity);
-window.alert(typeof null);
+n = "google"
+window.alert(typeof(n))
+window.alert(typeof 6.5)
+window.alert(typeof "6.5")
+window.alert(typeof function(){})
+window.alert(typeof undefined)
+window.alert(typeof NaN)
+window.alert(typeof Infinity)
+window.alert(typeof null)
