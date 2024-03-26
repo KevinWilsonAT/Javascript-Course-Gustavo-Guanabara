e70a942701217262cfd8c7e599b6ab9a553c9338
Kevin Wilson <104521683+KevinWilsonAT@users.noreply.github.com>
2024-03-26 12:03:24 -0300

    chore: Update classes 08-03-24
    anotation review

--- Module D - Conditions in JavaScript/aula011/ex014.js
+++ Module D - Conditions in JavaScript/aula011/ex014.js
@@ -1,17 +1,20 @@
 /*
-    SWITCH:
+    SWITCH CASE:
 
         Syntax:
             switch(<expression>){
                 case <option1>:
                     <command1>
                 break
+
                 case <option2>:
                     <command2>
                 break
+
                 case <option3>:
                     <command3>
                 break
+
                 default:
                     <command_default>
             }
