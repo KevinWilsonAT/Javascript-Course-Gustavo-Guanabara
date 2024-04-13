3206866012b48934b3c3ecf93c3e9e068ede989f
Kevin Wilson <104521683+KevinWilsonAT@users.noreply.github.com>
2024-04-13 13:48:26 -0300

    chore: upload class 13-04-2024

---
+++ Module F - Advancing studies in JavaScript/aula019/ex025.js
@@ -0,0 +1,35 @@
+/*
+    COMPOSE VARIABLES (VECTOR/ARRAY):
+
+        Syntax:
+			<array_name>[<field1>, <field2>, <field3>, ..., <fieldN>]
+
+		Atributtion (Specific Position)
+			<array_name>[<field>] = <value>
+
+		Atributtion (Last Position)
+			<array_name>.push(<value>)
+
+		Array Size
+			<array_name>.length
+
+		Array order (Increasing)
+			<array_name>.sort()
+
+		Array display
+
+			for(let <index_name> = <initial_position>; <index_name> < <array_name>.length>; <index_name><step>){
+				console.log(<array_name>[<index_name>])
+			}
+
+			Simplifying:
+
+			for(let <index_name> in <array_name>){
+				console.log(<array_name>[<index_name>])
+			}
+
+		Array Display (POSITION)
+			<array_name>.indexOf(<value>)
+
+
+*/
